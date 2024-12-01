9af39e6228c6514249069d3416a3bd4903237a07
RISHABH JAIN MODI <131861310+Rishabhbrain@users.noreply.github.com>
2024-12-02 03:15:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+let chatrooms = {};
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('join', ({ code, username }) => {
+        if (!chatrooms[code]) {
+            chatrooms[code] = [];
+        }
+
+        chatrooms[code].push(socket);
+        socket.username = username; // Store the username in the socket
+        socket.emit('joined', code);
+    });
+
+    socket.on('message', (message) => {
+        const code = message.code;
+        const text = message.text;
+
+        if (chatrooms[code]) {
+            chatrooms[code].forEach((client) => {
+                client.emit('message', { text, username: socket.username }); // Send username with the message
+            });
+        } else {
+            console.log(`Chat room ${code} does not exist`);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
